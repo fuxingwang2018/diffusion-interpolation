43064c61f82dcf7948a020a416ee60f9248bd6c0
dyazgi <daniel.yazgi@smhi.se>
2025-09-16 15:13:59 +0200

    change on how to submit

--- submit_meps.sh
+++ submit_meps.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=4                         # number of tasks
 #SBATCH --ntasks-per-node=4                # number of tasks per node
 #SBATCH --gpus-per-task=1                  # number of gpu per task
-#SBATCH --cpus-per-task=1                  # number of cores per task
+#SBATCH --cpus-per-task=4                  # number of cores per task
 #SBATCH --time=00:15:00                    # time (HH:MM:SS)
 #SBATCH --partition=gpu                    # partition
 #SBATCH --account=p200177                  # project account
@@ -20,16 +20,21 @@ set -x
 
 DATA_DIR=/home/users/u101329/p200177_t2/DE_371/datasets/datasets_SMHI/npy_intep
 SIF_FILE=../containers/container.sif 
-srun apptainer exec \
+srun --gpu-bind=single:1  --label bash -lc '
+  echo "RANK=$SLURM_PROCID LOCAL_RANK=$SLURM_LOCALID HOST=$HOSTNAME CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+  apptainer exec \
     --nv \
     --containall \
+    --env CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES \
     --bind ./work:/work \
-    --bind $DATA_DIR/samples:/samples \
-    --bind $DATA_DIR:/data \
+    --bind '"$DATA_DIR"'/samples:/samples \
+    --bind '"$DATA_DIR"':/data \
     --bind .:/code \
-    $SIF_FILE \
-    bash -c "
-        set -e
-        cd /code       
-        python train.py # test_meps_numpy_datamodule.py 
+    '"$SIF_FILE"' \
+    bash -lc "
+      set -e
+      cd /code
+      nvidia-smi --query-gpu=index,name,memory.total,memory.used --format=csv -i \$CUDA_VISIBLE_DEVICES
+      python -u train.py
     "
+'  # End of srun
