7a094c9ed70959ffa5f0fb6c8d2b42d278436297
dyazgi <daniel.yazgi@smhi.se>
2025-10-29 13:11:02 +0100

    revision plots

---
+++ generate/gen_2024_shuf-ruvt-40-steps.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e 
+set -x 
+
+
+export DATA_DIR=/home/users/u101329/p200177_t1_hp/u101329/npy_interp
+export OUT_DIR=/home/users/u101329/p200177_t1_hp/u101329/results/samples_2024_from_rutv_40-steps #
+
+export PYTHONPATH=`pwd`:$PYTHONPATH
+members=${1:ERROR}
+device=${2:ERROR}
+
+
+module load env/release/2024.1
+module load Python/3.12.3-GCCcore-13.3.0 
+source .venv/bin/activate
+python generate/sample.py --config-path `pwd`/conf -cn ddp_config_generate-4ch.yaml \
+    ckpt_file='/home/users/u101329/p200177_t2/u101329/diffusion-interp/_work/ddpm-npy-ruvt-20250929_134920/checkpoints/last.ckpt' \
+    outdir="${OUT_DIR}" \
+    datamodule.sequences_csv=${DATA_DIR}/sequences-2023-2024.csv  \
+    datamodule.date_range='["2024-10-22","2024-12-31"]' \
+    datamodule.members="${members}" \
+    device="${device}"
---
+++ generate/gen_2025-uvt2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e 
+set -x 
+
+
+export DATA_DIR=/home/users/u101329/p200177_t1_hp/u101329/npy_interp
+export OUT_DIR=/home/users/u101329/p200177_t1_hp/u101329/results/samples_utv_2025_2 #
+
+export PYTHONPATH=`pwd`:$PYTHONPATH
+range=${1:ERROR}
+device=${2:ERROR}
+
+
+module load env/release/2024.1
+module load Python/3.12.3-GCCcore-13.3.0 
+source .venv/bin/activate
+python generate/sample.py --config-path `pwd`/conf -cn ddp_config_generate.yaml \
+    ckpt_file='/home/users/u101329/p200177_t2/u101329/diffusion-interp/_work/ddpm-npy-ruvt-20250929_151421/checkpoints/last.ckpt' \
+    outdir=${OUT_DIR} \
+    datamodule.sequences_csv=${DATA_DIR}/sequences-2025.csv  \
+    datamodule.date_range="${range}" \
+    datamodule.members="[0]" \
+    device=${device}
